5d04209e8dd7094f1afd5966fe5df573184d804d
승완 <123719055+YooooSW@users.noreply.github.com>
2024-08-08 09:14:51 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,3 +1,6 @@
 def solution(num_list):
-    num_list.reverse()
-    return num_list
+    answer = []
+    for i in range(len(num_list)):
+        a = num_list[len(num_list) - (i+1)]
+        answer.append(a)
+    return answer
